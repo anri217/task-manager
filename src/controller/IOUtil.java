145e1327cebc3215e8befb7ee0da81b3a5ce8e2c
Nikita Govokhin <33430830+NikitaDestrain@users.noreply.github.com>
2019-11-19 19:02:17 +0400

    Update IOUtil.java

--- src/controller/IOUtil.java
+++ src/controller/IOUtil.java
@@ -3,10 +3,11 @@ package controller;
 import java.io.*;
 import model.*;
 
-
 public class IOUtil {
     public static void serializeObject (JTasks jtasks, OutputStream out) throws IOException {
         if (jtasks != null) {
+            // todo close stream or use try with resource for autocloseable objects
+            // research: is the next object autocloseable?
             ObjectOutputStream oos = new ObjectOutputStream(out);
             oos.writeObject(jtasks);
         }
