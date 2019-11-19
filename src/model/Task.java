d821c3414b57dfcb67c39bb7e8154a993d4f2b2a
Andrei Riazanov <Apple556@yandex.ru>
2019-11-19 13:34:47 +0400

    Modify Task.java

--- src/model/Task.java
+++ src/model/Task.java
@@ -10,4 +10,43 @@ public class Task {
     private Status status;
     private int id;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getDatePlan() {
+        return datePlan;
+    }
+
+    public void setDatePlan(Date datePlan) {
+        this.datePlan = datePlan;
+    }
+
+    public Date getDateDone() {
+        return dateDone;
+    }
+
+    public void setDateDone(Date dateDone) {
+        this.dateDone = dateDone;
+    }
 }
