30cd00a4f4e6974cab99be59115d1a0f71040efd
Andrei Riazanov <Apple556@yandex.ru>
2019-11-19 07:05:41 +0400

    Filled Alert.java

--- src/model/Alert.java
+++ src/model/Alert.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.util.Date;
+
 public class Alert {
+    private Date dateAlert;
+    private String textAlert;
+
+    public Date getDateAlert() {
+        return dateAlert;
+    }
+
+    public void setDateAlert(Date dateAlert) {
+        this.dateAlert = dateAlert;
+    }
+
+    public String getTextAlert() {
+        return textAlert;
+    }
+
+    public void setTextAlert(String textAlert) {
+        this.textAlert = textAlert;
+    }
 }
