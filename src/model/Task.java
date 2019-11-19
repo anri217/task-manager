62bded2d55fc8ed2091bee2e464608763f636f9c
Andrei Riazanov <Apple556@yandex.ru>
2019-11-19 07:14:42 +0400

    Filled Task.java

---
+++ src/model/Task.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Date;
+
+public class Task {
+    private String name;
+    private String description;
+    private Date datePlan;
+    private Date dateDone;
+    private Status status;
+    private int id;
+
+}
