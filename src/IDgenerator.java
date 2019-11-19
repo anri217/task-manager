a96892444a065485f4d908c15fd36e41c0571bd2
Nikita Govokhin <33430830+NikitaDestrain@users.noreply.github.com>
2019-11-19 19:04:29 +0400

    Update IDgenerator.java

--- src/IDgenerator.java
+++ src/IDgenerator.java
@@ -1,6 +1,7 @@
 
 // todo nigo is it work? looks like you do not try to generate value
 // also please use Code Convention
+// see requirements in issue for this class
 public class IDgenerator {
     private static int id = 0;
     public int  getId() {
