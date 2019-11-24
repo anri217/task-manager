3b1d5f679f793eff9eec9088afea3c921222ae15
Ilya <il.kozlov@gmail.com>
2019-11-24 17:27:43 +0400

    Modify Notifier, add getId and setId in Task.

--- src/model/Notifier.java
+++ src/model/Notifier.java
@@ -1,21 +1,50 @@
 package model;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Notifier {
+    private Map<Integer, TimerTask> notificationMap;
     private Timer timer;
     private TimerTask timerTask;
 
-    public Notifier(Notification alr) {
+    public Notifier() {
+        notificationMap = new LinkedHashMap<>();
+    }
+
+    public void createTask(Task task, Timer timerr){
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Оповещение о задаче "+task.getName());
+                timerr.cancel();
+            }
+        };
+    }
+    public void addNotification(Task task) {
+        notificationMap.putIfAbsent(task.getId(), timerTask);
+    }
+
+    public void deleteNotification(Task task){
+        notificationMap.remove(task.getId());
+    }
+
+
+    public TimerTask getNotification(int id) {
+        return notificationMap.get(id);
+    }
+
+    public void createTimer(Task task, Notification alr) {
+        Timer timerr = new Timer();
+        createTask(task, timerr);
+        addNotification(task);
+        timerr.schedule(notificationMap.get(task.getId()), alr.getDateAlert());
+    }
+
+    public void createTimer(Notification alr, TimerTask tTask) {
         timer = new Timer();
-    }
-
-    public void CreateTimer(Notification alr) {
-        timer.schedule(timerTask, alr.getDateAlert());
-    }
-
-    public void CreateTimer(Notification alr, TimerTask tTask) {
         timer.schedule(tTask, alr.getDateAlert());
     }
 
@@ -23,8 +52,8 @@ public class Notifier {
         return this.timer;
     }
 
-    public void setTimer(Timer utimer) {
-        timer = utimer;
+    public void setTimer(Timer usertimer) {
+        timer = usertimer;
     }
 
     public void setTimerTask(TimerTask tTask) {
@@ -35,3 +64,4 @@ public class Notifier {
         return this.timerTask;
     }
 }
+
