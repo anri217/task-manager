c3b370b0826afd41b66bf6bf7cb913d28a7d9657
Andrei Riazanov <Apple556@yandex.ru>
2019-11-27 21:35:40 +0400

    Modify MainWindow.java

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -2,15 +2,8 @@ package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
