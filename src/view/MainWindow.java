62256308145fe90d20a26dc4a2bf52330b2dd31b
Andrei Riazanov <Apple556@yandex.ru>
2019-11-27 21:35:11 +0400

    Modify MainWindow.java and add mainWindow.fxml

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1,7 +1,9 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -11,7 +13,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-
 public class MainWindow extends Application {
     public static void main(String args[]) {
         Application.launch(args);
@@ -19,90 +20,9 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        GridPane root = new GridPane();
-        GridPane root1 = new GridPane();
-        root.setStyle("-fx-background-color:#df665d"); //kkk
-
-        root.setPadding(new Insets(20));
-        root.setHgap(25);
-        root.setVgap(15);
-
-        Label labelTitle = new Label("ЖУРНАЛ ЗАДАЧ");
-        labelTitle.setFont(new Font("Impact", 30));
-        labelTitle.setTextFill(Color.web("#ffcf04"));
-        root.add(labelTitle, 0, 0, 2, 1);
-
-        Button exit = new Button("Выйти");
-        root.add(exit, 4, 7);
-
-        Button change = new Button("Изменить");
-        root.add(change, 2, 7);
-
-        Button delTask = new Button("Удалить задачу");
-        root.add(delTask, 1, 7);
-
-        Button addTask = new Button("Добавить задачу");
-        root.add(addTask, 0, 7);
-
-        Button fileButton = new Button("File");
-        root.add(fileButton, 0, 1);
-
-        Label checkBoxes = new Label("№");
-        checkBoxes.setFont(new Font("Arial Black", 20));
-        root.add(checkBoxes, 0, 2);
-
-        Label nameTask = new Label("Наименование");
-        nameTask.setFont(new Font("Arial Black", 20));
-        root.add(nameTask, 1, 2);
-
-        Label descriptionLabel = new Label("Описание");
-        descriptionLabel.setFont(new Font("Arial Black", 20));
-        root.add(descriptionLabel, 2, 2);
-
-        Label dateLabel = new Label("Дата");
-        dateLabel.setFont(new Font("Arial Black", 20));
-        root.add(dateLabel, 3, 2);
-
-        CheckBox checkBox1 = new CheckBox();
-        root.add(checkBox1, 0, 3);
-
-        CheckBox checkBox2 = new CheckBox();
-        root.add(checkBox2, 0, 4);
-
-        CheckBox checkBox3 = new CheckBox();
-        root.add(checkBox3, 0, 5);
-
-        Label name1 = new Label("Позавтракать");
-        root.add(name1, 1, 3);
-
-        Label name2 = new Label("Погулять с собакой");
-        root.add(name2, 1, 4);
-
-        Label name3 = new Label("Пообедать");
-        root.add(name3, 1, 5);
-
-        Label desc1 = new Label("Съесть омлет и выпить кофе");
-        root.add(desc1, 2, 3);
-
-        Label desc2 = new Label("Прогулка с собакой по парку");
-        root.add(desc2, 2, 4);
-
-        Label desc3 = new Label("Съесть борщ");
-        root.add(desc3, 2, 5);
-
-        Label date1 = new Label("09.01.2007 9:41");
-        root.add(date1, 3, 3);
-
-        Label date2 = new Label("09.01.2007 9:41");
-        root.add(date2, 3, 4);
-
-        Label date3 = new Label("09.01.2007 9:41");
-        root.add(date3, 3, 5);
-
-        Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
         stage.setTitle("TASK MANAGER");
-        stage.setScene(scene);
-        stage.setResizable(false);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 }
