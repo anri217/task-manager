bd4de7c42daeb3c07592705ca38a0f2b36fc6321
Andrei Riazanov <Apple556@yandex.ru>
2019-12-06 02:24:31 +0400

    delete lib

--- src/IdGenerator.java
+++ src/IdGenerator.java
@@ -2,14 +2,9 @@
 // todo nigo is it work? looks like you do not try to generate value
 // also please use Code Convention
 // see requirements in issue for this class
-public class IdGenerator {
+public class IDgenerator {
     private static int id = 0;
-
-    public IdGenerator(int id) {
-        this.id = id;
-    }
-
-    static public int getId() {
+    public int  getId() {
         return id++;
     }
 }
