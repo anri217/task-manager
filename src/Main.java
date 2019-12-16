e8caaeff9d2abdbba58ae6bf047cc2884751c54e
Andrei Riazanov <Apple556@yandex.ru>
2019-12-17 02:00:19 +0400

    changeWindow

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
-import controller.IOUtil;
-import javafx.application.Application;
-import model.Journal;
 import view.MainWindow;
 
-import java.io.IOException;
-
 public class Main {
-  public static void main(String[] args) throws IOException, ClassNotFoundException {
-//
-//    IOUtil ioUtil = IOUtil.getInstance();                             //begin
-//
-//    Journal journal = (Journal)ioUtil.restoreFunction();
-//
-//    ioUtil.backupFunction(journal);                                   //end
-    MainWindow.run(args);
-  }
+  public static void main(String[] args) { MainWindow.run(args); }
 }
