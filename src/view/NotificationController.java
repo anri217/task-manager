ea25a5316a6282a8e295e19383731ab7a82def55
JustTrag <il.kozlov98@gmail.com>
2019-12-17 02:02:10 +0400

    Done NotificationWindow with Defer Task and Finish Task

--- src/view/NotificationController.java
+++ src/view/NotificationController.java
@@ -6,7 +6,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import model.Notification;
@@ -14,6 +16,8 @@ import model.Status;
 import model.Task;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class NotificationController {
 
@@ -26,6 +30,19 @@ public class NotificationController {
     private Button deferButton;
     @FXML
     private Button finishButton;
+    @FXML
+    private Label setDateLabel;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private TextField hoursNewTextField;
+    @FXML
+    private TextField minutesNewTextField;
+    @FXML
+    private Label pointsLabel;
+    @FXML
+    private Button deferTaskButton;
+
 
     public NotificationController(Notification notification){
         this.notification = notification;
@@ -54,13 +71,31 @@ public class NotificationController {
     public void finishTaskAction(ActionEvent actionEvent) {
         Task finishTask = notification.getTask();
         finishTask.setStatus(Status.FINISHED);
+        finishTask.setDateOfDone(notification.getTask().getPlannedDate());
         Controller.getInstance().changeTask(notification.getTask().getId(), finishTask);
         Stage stage = (Stage) finishButton.getScene().getWindow();
         stage.close();
     }
 
+    @FXML
     public void deferButtonAction(ActionEvent actionEvent){
+        descLabel.setVisible(false);
+        finishButton.setVisible(false);
+        deferButton.setVisible(false);
+        deferTaskButton.setVisible(true);
+        setDateLabel.setVisible(true);
+        datePicker.setVisible(true);
+        hoursNewTextField.setVisible(true);
+        pointsLabel.setVisible(true);
+        minutesNewTextField.setVisible(true);
+    }
 
+    public void deferTaskButtonAction(ActionEvent actionEvent){
+        Task deferTask = notification.getTask();
+        deferTask.setPlannedDate(LocalDateTime.of(datePicker.getValue().getYear(), datePicker.getValue().getMonthValue(), datePicker.getValue().getDayOfMonth(), Integer.parseInt(hoursNewTextField.getText()), Integer.parseInt(minutesNewTextField.getText())));
+        Controller.getInstance().changeTask(notification.getTask().getId(), deferTask);
+        Stage stage = (Stage) deferButton.getScene().getWindow();
+        stage.close();
     }
 
 
