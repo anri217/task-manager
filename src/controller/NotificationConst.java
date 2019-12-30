521c04132a44f373d2987d34973176b9d231164a
Nikita Govokhin <33430830+NikitaDestrain@users.noreply.github.com>
2019-12-30 13:39:01 +0400

    Update NotificationConst.java

--- src/controller/NotificationConst.java
+++ src/controller/NotificationConst.java
@@ -1,6 +1,8 @@
 package controller;
 
+// todo please, use full words for class names
 public class NotificationConst {
+    // todo read https://www.oracle.com/technetwork/articles/javaee/codeconventions-135099.html
     public final static String NOTIFICATIONWINDOWPATH = "../view/notificationWindow.fxml";
     public final static String NOTIFICATIONTITLE = "NOTIFICATION";
 
