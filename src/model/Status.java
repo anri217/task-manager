507c16e24d97add6118f5beb23e6afff5e98d566
Andrei Riazanov <Apple556@yandex.ru>
2019-12-30 11:36:27 +0400

    /

--- src/model/Status.java
+++ src/model/Status.java
@@ -2,5 +2,5 @@ package model;
 
 // todo not all statuses
 public enum Status {
-    PLANNED, COMPLETED, OVERDUE, CANCELED, DEFFERED; //add new status
+    PLANNED, COMPLETED, OVERDUE, CANCELED, DEFERRED; //add new status
 }
