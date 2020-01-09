245bc02f826a640ba3de7063b685c4b7a89c9cad
Andrei Riazanov <Apple556@yandex.ru>
2020-01-10 01:07:15 +0400

    д

--- src/IdGenerator.java
+++ src/IdGenerator.java
@@ -2,7 +2,9 @@
 // todo nigo is it work? looks like you do not try to generate value
 // also please use Code Convention
 // see requirements in issue for this class
+public class IDgenerator {
     private static int id = 0;
+    public int  getId() {
         return id++;
     }
 }
