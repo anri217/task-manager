64dc25404cc3b741993497d4a3fd870de17d1ede
Andrei Riazanov <Apple556@yandex.ru>
2020-01-31 14:57:06 +0400

    client changes

--- src/client/ClientFacade.java
+++ src/client/ClientFacade.java
@@ -2,13 +2,20 @@ package client;
 
 
 public class ClientFacade {
-
-
-
     void sendCommand(String command) {
-
+        //build json and send to server json file
+        //may be we can separate this method like addTask, changeTask and etc
     }
 
+    void refresh(){
+        //refreshing table
+    }
 
+    void showNotification(){
+        //show notification, in args should be task
+    }
 
+    void sendAll(){
+        //sending all table items to server
+    }
 }
---
+++ src/client/jsonBuilder.java
@@ -0,0 +1,5 @@
+package client;
+
+public class jsonBuilder {
+    //for building correct json
+}
