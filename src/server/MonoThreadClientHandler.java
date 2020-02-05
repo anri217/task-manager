1b71b844e5e8b8a25b44782ebe0b02e2e88cb453
Andrei Riazanov <Apple556@yandex.ru>
2020-02-05 18:29:38 +0400

    .

--- src/server/MonoThreadClientHandler.java
+++ src/server/MonoThreadClientHandler.java
@@ -20,51 +20,30 @@ public class MonoThreadClientHandler implements Runnable {
             DataOutputStream out = new DataOutputStream(clientDialog.getOutputStream());
             DataInputStream in = new DataInputStream(clientDialog.getInputStream());
 
-
             while (!clientDialog.isClosed()) {
                 System.out.println("Server reading from channel");
                 String entry = in.readUTF();
 
-
                 System.out.println("READ from clientDialog message - " + entry);
-                // инициализация проверки условия продолжения работы с клиентом
-                // по этому сокету по кодовому слову - quit в любом регистре
-                if (entry.equalsIgnoreCase("quit")) {
 
-                    // если кодовое слово получено то инициализируется закрытие
-                    // серверной нити
+                if (entry.equalsIgnoreCase("quit")) {
                     System.out.println("Client initialize connections suicide ...");
                     out.writeUTF("Server reply - " + entry + " - OK");
                     break;
                 }
 
-                // если условие окончания работы не верно - продолжаем работу -
-                // отправляем эхо обратно клиенту
-
                 System.out.println("Server try writing to channel");
                 out.writeUTF("Server reply - " + entry + " - OK");
                 out.flush();
                 System.out.println("Server Wrote message to clientDialog.");
-
-                // освобождаем буфер сетевых сообщений
-
-
-                // возвращаемся в началло для считывания нового сообщения
             }
 
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            // основная рабочая часть //
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            // если условие выхода - верно выключаем соединения
             System.out.println("Client disconnected");
             System.out.println("Closing connections & channels.");
 
-            // закрываем сначала каналы сокета !
             in.close();
             out.close();
 
-            // потом закрываем сокет общения с клиентом в нити моносервера
             clientDialog.close();
 
             System.out.println("Closing connections & channels - DONE.");
--- src/server/MultiThreadServer.java
+++ src/server/MultiThreadServer.java
@@ -16,22 +16,11 @@ public class MultiThreadServer {
      * @param args
      */
     public static void main(String[] args) {
-
-        // стартуем сервер на порту 3345 и инициализируем переменную для обработки консольных команд с самого сервера
         try (ServerSocket server = new ServerSocket(3345);
              BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-
-
-            // стартуем цикл при условии что серверный сокет не закрыт
             while (!server.isClosed()) {
-
-                // проверяем поступившие комманды из консоли сервера если такие
-                // были
                 if (br.ready()) {
                     System.out.println("Main Server found any messages in channel, let's look at them.");
-
-                    // если команда - quit то инициализируем закрытие сервера и
-                    // выход из цикла раздачии нитей монопоточных серверов
                     String serverCommand = br.readLine();
                     if (serverCommand.equalsIgnoreCase("quit")) {
                         System.out.println("Main Server initiate exiting...");
@@ -40,19 +29,12 @@ public class MultiThreadServer {
                     }
                 }
 
-
                 Socket client = server.accept();
 
-                // после получения запроса на подключение сервер создаёт сокет
-                // для общения с клиентом и отправляет его в отдельную нить
-                // в Runnable(при необходимости можно создать Callable)
-                // монопоточную нить = сервер - MonoThreadClientHandler и тот
-                // продолжает общение от лица сервера
                 executeIt.execute(new MonoThreadClientHandler(client));
                 System.out.print("Connection accepted.");
             }
 
-            // закрытие пула нитей после завершения работы всех нитей
             executeIt.shutdown();
         } catch (IOException e) {
             e.printStackTrace();
