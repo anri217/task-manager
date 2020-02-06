3b05e6bddc679fc05bde9439d8ffa0f600cca177
Andrei Riazanov <Apple556@yandex.ru>
2020-02-06 07:20:27 +0400

    /

--- src/client/CommandCreator.java
+++ src/client/CommandCreator.java
@@ -20,7 +20,6 @@ public class CommandCreator {
         setTask_count(task_count);
         setCommand_id(command_id);
     }
-    //
 
     public void CreateJson() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
