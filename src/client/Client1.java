e2b3524f49728530a556e0bb9ae6ccf786c4448b
Desire456 <zipzap1337>
2020-02-17 20:40:40 +0400

    Add second client for testing multithread-server

---
+++ src/client/Client1.java
@@ -0,0 +1,22 @@
+package client;
+
+import client.view.mainWindow.MainWindow;
+import server.controller.utils.Paths;
+import server.controller.utils.PropertyParser;
+import server.exceptions.PropertyParserInitException;
+
+import java.io.IOException;
+
+public class Client1 {
+    public static void main(String[] args) throws PropertyParserInitException {
+        try {
+            PropertyParser propertyParser = new PropertyParser(Paths.CLIENT);
+
+            ClientFacade clientFacade = new ClientFacade(propertyParser.getProperty("host"),
+                    Integer.parseInt(propertyParser.getProperty("port")));
+            clientFacade.connect(args);
+        } catch (PropertyParserInitException | IOException ex) {
+            throw new PropertyParserInitException(ex.getMessage());
+        }
+    }
+}
