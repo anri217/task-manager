127d45521b805e02207c9600dc98eb3e6ae983db
Desire456 <zipzap1337>
2020-02-19 14:36:26 +0400

    /

---
+++ src/client/handlers/TakeAllTasksFromStartHandler.java
@@ -0,0 +1,66 @@
+package client.handlers;
+
+import client.view.MainWindowRow;
+import client.view.RefreshHelper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import server.TaskConverter;
+import server.controller.factories.TaskFactory;
+import shared.Command;
+import shared.Handler;
+import shared.model.Status;
+import shared.model.Task;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class TakeAllTasksFromStartHandler implements Handler {
+    private ArrayList<Task> tasks;
+
+    @Override
+    public void handle(Command command) throws JsonProcessingException {
+        tasks = new ArrayList<Task>();
+        List<LinkedHashMap<String, Object>> taskList = (List) command.getContent();
+        for (int i = 0; i < taskList.size(); i++) {
+            tasks.add(TaskConverter.getInstance().convert(taskList.get(i)));
+        }
+        for (Task task : tasks) {
+            if (task.getDateOfDone() == null) {
+                if (task.getPlannedDate().isBefore(LocalDateTime.now())) {
+                    task.setStatus(Status.OVERDUE);
+                }
+            }
+        }
+        ArrayList<MainWindowRow> rows = new ArrayList<MainWindowRow>();
+        for (Task task : tasks) {
+            rows.add(new MainWindowRow(task));
+        }
+        RefreshHelper.getInstance().getMainWindowController().setRows(rows);
+        RefreshHelper.getInstance().getMainWindowController().refresh();
+    }
+
+    private void createTask(LinkedHashMap<String, Object> map) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime plannedDate = LocalDateTime.parse((String) map.get("plannedDate"), formatter);
+        TaskFactory taskFactory = new TaskFactory();
+        Task task = taskFactory.createTask((int) map.get("id"), (String) map.get("name"), (String) map.get("description"), plannedDate, chooseStatus((String) map.get("status")));
+        tasks.add(task);
+    }
+
+    private Status chooseStatus(String status) {
+        if (status.equals("PLANNED"))
+            return Status.PLANNED;
+        else {
+            if (status.equals("COMPLETED")) return Status.COMPLETED;
+            else {
+                if (status.equals("OVERDUE")) return Status.OVERDUE;
+                else {
+                    if (status.equals("CANCELED")) return Status.CANCELED;
+                    else return Status.DEFERRED;
+                }
+            }
+        }
+    }
+}
