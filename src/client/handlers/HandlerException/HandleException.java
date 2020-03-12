b0e1c576888c05c24852c7fa7fef48a687c882b8
Nikita Govokhin <33430830+NikitaDestrain@users.noreply.github.com>
2020-03-12 14:31:39 +0400

    Update HandleException.java

--- src/client/handlers/HandlerException/HandleException.java
+++ src/client/handlers/HandlerException/HandleException.java
@@ -2,7 +2,7 @@ package client.handlers.HandlerException;
 
 import java.io.IOException;
 
-public class HandleException extends IOException {
+public class HandleException extends IOException { // todo why does it extend IOException?
 
     public HandleException(Throwable e){
         initCause(e);
